3d9602f7c52e4ee76b532b2941e1ec0abbe619ff
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-04-19 19:45:23 -0300

    Adicionando uma pasta para guardar as imagens

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
+/*+=============================================================
+2 | UNIFAL = Universidade Federal de Alfenas.
+3 | BACHARELADO EM CIENCIA DA COMPUTACAO.
+4 | Trabalho . . : Imagem ASCII
+5 | Disciplina . : Processamento de Imagens
+6 | Professor. . : Luiz Eduardo da Silva
+7 | Aluno . . . .: Gabriel Francelino Nascimento
+8 | Data . . . . : ??/04/2023
+9 +=============================================================*/
+
 public class Main {
 
   public static void main(String[] args) {
--- makefile
+++ makefile
@@ -1,14 +1,11 @@
-JAVAC = javac
-JAVA = java
-MAIN_CLASS = Main
+JAVAC=javac
+JAVA=java
+MAIN_CLASS=Main
 
 all: compile 
 
-compile:\
-    $(JAVAC) $(MAIN_CLASS).java
+compile: $(JAVAC) $(MAIN_CLASS).java
 
-run:\
-    $(JAVA) $(MAIN_CLASS) 
+run: $(JAVA) $(MAIN_CLASS) 
 
-clean:\
-    rm -f *.class
+clean: rm -f *.class
